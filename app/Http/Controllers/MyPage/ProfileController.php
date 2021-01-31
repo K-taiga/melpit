700c44c16f2506efb55e18f29a6e4dac942f379d
K-taiga <taigakatou0329@gmail.com>
2021-01-31 17:04:46 +0900

    add editProfile method

--- app/Http/Controllers/MyPage/ProfileController.php
+++ app/Http/Controllers/MyPage/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\MyPage;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\Mypage\Profile\EditRequest;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
@@ -13,5 +14,17 @@ class ProfileController extends Controller
         return view('mypage.profile_edit_form')
         // bladeにAuth::userでログインしているユーザーをuserで渡す
             ->with('user',Auth::user());
+    }
+
+    public function editProfile(EditRequest $request) {
+        $user = Auth::user();
+
+        $user->name = $request->input('name');
+        $user->save();
+
+        // 直前のページにリダイレクトする
+        return redirect()->back()
+        // statusに文字列を入れ返す
+            ->with('status','プロフィールを変更しました。');
     }
 }
