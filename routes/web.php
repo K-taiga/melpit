03eb3d03e741178915b1edc566dbdb3010cb76d5
K-taiga <taigakatou0329@gmail.com>
2021-01-31 15:10:38 +0900

    add register.view

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+})->name('top');
 
 Auth::routes();
 
