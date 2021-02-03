d15efd668d774726e2111a39411b1d5e80ff3ccd
K-taiga <taigakatou0329@gmail.com>
2021-02-04 08:29:27 +0900

    add 画像のリサイズと保存処理

--- app/Http/Controllers/MyPage/ProfileController.php
+++ app/Http/Controllers/MyPage/ProfileController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\MyPage;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Mypage\Profile\EditRequest;
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Image;
 
 class ProfileController extends Controller
 {
@@ -21,11 +25,38 @@ class ProfileController extends Controller
         $user = Auth::user();
 
         $user->name = $request->input('name');
+
+        if($request->has('avatar')){
+            $fileName = $this->saveAvatar($request->file('avatar'));
+            $user->avatar_file_name = $fileName;
+        }
+
         $user->save();
 
         // 直前のページにリダイレクトする
         return redirect()->back()
         // statusに文字列を入れ返す
             ->with('status','プロフィールを変更しました。');
+    }
+    
+    /**
+     * saveAvatar アバター画像をリサイズして保存する
+     *
+     * @param  mixed $file
+     * @return string
+     */
+    private function saveAvatar(UploadedFile $file) :string
+    {
+        // 一時ファイル生成時用のパスを取得
+        $tempPath = $this->makeTempPath();
+
+        // リサイズして一時保存
+        Image::make($file)->fit(200,200)->save($tempPath);
+
+        // Storageファサードで画像をpuclicのdiskに保存する
+        // 他にlocal,S3のdiskが選べる
+        $filePath = Storage::disk('public')->putFile('avatars',new File($tempPath));
+
+        return basename($filepath);
     }
 }
