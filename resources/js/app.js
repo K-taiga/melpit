5cf08bd05e0b1399eaf62e3651d2c362a56eb3d5
K-taiga <taigakatou0329@gmail.com>
2021-02-05 07:52:20 +0900

    add 画像周りのフロント

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+document.querySelector('.image-picker input')
+    .addEventListener('change',(e) => {
+        const input = e.target;
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            // inputタグから親方向であるimageクラスのDOMを探しそこから更にimgタグのDOMを検索
+            // e.target.resultは画像データをbase64エンコードしている
+            input.closest('.image-picker').querySelector('img').src = e.target.result
+        };
+        reader.readAsDataURL(input.files[0]);
+    });
